78db8c5818375d13e67b17be541feadca84ebd1e
icarusw-code <77170611+icarusw-code@users.noreply.github.com>
2022-01-11 13:29:10 +0900

    use filter

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
-        basePackages =
+
         // 기존 코드를 남기기 위해 예외처리 함
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
